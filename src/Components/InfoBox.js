733d0d8ae9b5f44c6a0fed4206a7ac2f7fa956bd
Nico83code <nicoman@gmail.com>
2020-10-20 18:17:47 +0200

    graph, map tracking done

--- src/Components/InfoBox.js
+++ src/Components/InfoBox.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
 import "./InfoBox.css";
-import { Card, CardContent, Typography } from '@material-ui/core';
 
-function InfoBox({ title, cases, total }) {
-    return (
-        <Card className='InfoBox'>
-            <CardContent>
-                <Typography className="infoBox__title" color="textSecondary">
-                    {title}
-                </Typography>
+function InfoBox({ title, cases, total, active, isRed, isGrey, ...props }) {
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"
+        } ${isGrey && "infoBox--grey"}`}
+    >
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {title}
+        </Typography>
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"} ${isGrey && "infoBox__cases--grey"}`}>
+          {cases}
+        </h2>
 
-                <h2 className="infoBox__cases">{cases}</h2>
 
-                <Typography className="infoBox__total" color="textSecondary">
-                    {total} Total
-                </Typography>
-            </CardContent>
-        </Card>
-    )
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card >
+  );
 }
 
-export default InfoBox
+export default InfoBox;
